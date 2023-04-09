ed08c531785d2ceffdef4e7f153dcdd8b817d86f
TamyrisRM <tamyrisrayani@gmail.com>
2023-04-09 19:32:19 -0300

    slide 13

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Frag from "./componentes/Frag";
 import ParImpar from "./componentes/ParImpar";
 import Familia from "./componentes/relacao/Familia";
 import Membro from "./componentes/relacao/Membro";
+import UsuarioLogado from "./componentes/UsuarioLogado";
 
 export default class App extends Component {
   render() {
@@ -15,10 +16,21 @@ export default class App extends Component {
         subTitulo="Tela de Cadastro de Produto"/>*/}
         {/*<ParImpar numero="9" />*/}
 
-        <Familia>
+        {/*<Familia>
             <Membro nome="Maria" sobreNome="Silva" />
-            <Membro nome="José" sobreNome="Souza" />
+            <Membro nome="José" sobreNome="Silva" />
         </Familia>
+
+        <Familia>
+          <Membro nome="Bianca" sobreNome="Cunha"/>
+          <Membro nome="Gustavo" sobreNome="Cunha"/>
+      </Familia>*/}
+
+      <UsuarioLogado usuario={ {nome:'teste', email:'teste@teste.com'}} />
+      <UsuarioLogado usuario={ {nome: 'teste 2'}}/>
+      <UsuarioLogado usuario={ {email:'teste@teste.com'}} />
+      <UsuarioLogado usuario= {null} />
+      <UsuarioLogado usuario={ {} } />
 
       </View>
     );
