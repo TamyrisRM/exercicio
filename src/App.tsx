ae6b4290a8b611b461c222abf40d0ea9d39f9216
TamyrisRM <tamyrisrayani@gmail.com>
2023-06-05 22:39:13 -0300

    05/06/23

--- src/App.tsx
+++ src/App.tsx
@@ -11,48 +11,13 @@ import Contador from "./componentes/Contador";
 import DigiteSeuNome from "./componentes/DigiteSeuNome";
 import DimensoesFixas from "./componentes/DimensoesFixas";
 import Mega from "./componentes/MegaSena/Mega";
-import Button from "./componentes/Calculadora/Button";
-import Display from "./componentes/Calculadora/Display";
+import Calculadora from "./componentes/Calculadora/Calculadora";
+
 
 export default class App extends Component {
-  state = {
-    displayValue: '0',
-  }
-
-  clearDisplay = () => {
-    this.setState({displayValue: '0'})
-  }
-  setDigit = (digito: any) => {
-    this.setState({displayValue: digito})
-  }
-  setOperation = (op: any) => {
-
-  }
   render() {
     return (
-      <View style={estilos.calculadora}>
-        <Display value={this.state.displayValue} />
-        <View style={estilos.buttons}>
-          <Button label='AC' triple onClick={() => this.clearDisplay()} />
-          <Button label='/' op onClick={() => this.setOperation('/')}/>
-          <Button label='7' onClick={this.setDigit} />
-          <Button label='8' onClick={this.setDigit}/>
-          <Button label='9' onClick={this.setDigit}/>
-          <Button label='*' op/>
-          <Button label='4' onClick={this.setDigit}/>
-          <Button label='5' onClick={this.setDigit}/>
-          <Button label='6' onClick={this.setDigit}/>
-          <Button label='-' op/>
-          <Button label='1' onClick={this.setDigit}/>
-          <Button label='2' onClick={this.setDigit}/>
-          <Button label='3' onClick={this.setDigit}/>
-          <Button label='+' op/>
-          <Button label='0' double/>
-          <Button label='.' onClick={this.setDigit}/>
-          <Button label='=' op/>
-          
-        </View>
-        {/*<View style={estilos.container}>
+      <View style={estilos.container}>
         {/*<Simples texto='Tamyris'/>*/}
         {/*<Frag titulo="Cadastro" 
         subTitulo="Tela de Cadastro de Produto"/>*/}
@@ -82,10 +47,12 @@ export default class App extends Component {
       
       <DimensoesFixas/>
 
-      <Mega qtdeNumeros={6} />
-      </View>*/}
-      </View>
-      
+    <Mega qtdeNumeros={6} />*/}
+
+    
+      <Calculadora />
+
+      </View> 
       
     );
   }
@@ -98,12 +65,5 @@ const estilos = StyleSheet.create({
   },
   fonte: {
     fontSize: 50,
-  },
-  calculadora: {
-    flex: 1,
-  },
-  buttons: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
   },
 })
